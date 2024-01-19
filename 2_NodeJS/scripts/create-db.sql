f89c6f3be50a887d9d195e6a23037e650b244a50
Guilherme Segers <guisegers@pop-os.localdomain>
2024-01-19 20:05:32 -0300

    Adicionado script de inicializacao ao Volume

--- 2_NodeJS/scripts/create-db.sql
+++ 2_NodeJS/scripts/create-db.sql
@@ -1,11 +1,5 @@
--- Cria a base de dados
-CREATE DATABASE db_desafios;
--- Entra na base de dados
-USE db_desafios
--- Cria table People
-CREATE TABLE people (id char(36) not null, firstName varchar(150), lastName varchar(150), nickName varchar(150), primary key(id));
-
--- Cria o usuario
-CREATE USER 'desafio'@'%' IDENTIFIED BY 'desafio123';
--- Da permissão para o usuario desafio as tabelas da database db_desafios
+CREATE DATABASE IF NOT EXISTS db_desafios;
+USE db_desafios;
+CREATE TABLE IF NOT EXISTS people (id char(36) not null, firstName varchar(150), lastName varchar(150), nickName varchar(150), primary key(id));
+CREATE USER IF NOT EXISTS 'desafio'@'%' IDENTIFIED BY 'desafio123';
 GRANT ALL PRIVILEGES ON db_desafios.* TO 'desafio'@'%' WITH GRANT OPTION;
